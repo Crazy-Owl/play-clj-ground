c101a8066aa9aaa29b24782e88feae006be87938
Crazy-Owl <stalker401@gmail.com>
2014-06-11 18:53:01 +0400

    two screens can be used - every one of them has different entities set

--- desktop/src-common/play_clj_ground/core.clj
+++ desktop/src-common/play_clj_ground/core.clj
@@ -7,13 +7,25 @@
   (fn [screen entities]
     (update! screen :renderer (stage))
     (label "Hello world!" (color :white)))
-  
+
   :on-render
   (fn [screen entities]
+    (println entities)
     (clear!)
+    (render! screen entities)))
+
+(defscreen overlay-screen
+  :on-show
+  (fn [screen entities]
+    (update! screen :renderer (stage))
+    (assoc (label "WWWWW" (color :green)) :x 100 :y 200 :pppp true))
+
+  :on-render
+  (fn [screen entities]
+    (println entities)
     (render! screen entities)))
 
 (defgame play-clj-ground
   :on-create
   (fn [this]
-    (set-screen! this main-screen)))
+    (set-screen! this main-screen overlay-screen)))
