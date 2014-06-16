958b5bc773f21d046bef7d439b1d462dbb99b065
Crazy-Owl <stalker401@gmail.com>
2014-06-16 20:15:59 +0400

    delta-time and fps getting

--- desktop/src-common/play_clj_ground/core.clj
+++ desktop/src-common/play_clj_ground/core.clj
@@ -1,6 +1,7 @@
 (ns play-clj-ground.core
   (:require [play-clj.core :refer :all]
-            [play-clj.ui :refer :all]))
+            [play-clj.ui :refer :all]
+            [play-clj.g2d :refer :all]))
 
 (defscreen main-screen
   :on-show
@@ -17,15 +18,27 @@
   (fn [screen entities]
     (println (str "in my-test-fn, arg is " (:arg screen)))))
 
+(defn update-labels ; update labels with frames-per-second and delta-time data
+  [entities]
+  (map (fn [entity]
+         (case (:id entity)
+           :fps (doto entity (label! :set-text (str (graphics! :get-frames-per-second))))
+           :dt (doto entity (label! :set-text (str (graphics! :get-delta-time))))
+           :else entity))
+       entities))
+
 (defscreen overlay-screen
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage))
-    (assoc (label "WWWWW" (color :green)) :x 100 :y 200 :pppp true))
+    [(assoc (label "0" (color :green)) :x 100 :y 200 :id :fps)
+     (assoc (label "0" (color :red)) :x 100 :y 250 :id :dt)])
 
   :on-render
   (fn [screen entities]
-    (render! screen entities))
+    (->> entities
+         update-labels
+         (render! screen)))
 
   :on-key-down
   (fn [screen entities]
