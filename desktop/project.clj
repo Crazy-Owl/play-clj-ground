00aeab05e4e5042dd25ed2b18635440be273db4a
Crazy-Owl <stalker401@gmail.com>
2014-06-18 18:56:46 +0400

    updated play-clj version

--- desktop/project.clj
+++ desktop/project.clj
@@ -1,6 +1,6 @@
 (defproject play-clj-ground "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  
+
   :dependencies [[com.badlogicgames.gdx/gdx "1.1.0"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.1.0"]
                  [com.badlogicgames.gdx/gdx-box2d "1.1.0"]
@@ -12,8 +12,8 @@
                  [com.badlogicgames.gdx/gdx-platform "1.1.0"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.3.6"]]
-  
+                 [play-clj "0.3.7"]]
+
   :source-paths ["src" "src-common"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [play-clj-ground.core.desktop-launcher]
