16b9864139707fc296dd441d1ebe03e30528ca21
Crazy-Owl <stalker401@gmail.com>
2014-07-22 14:09:36 +0400

    modified protocols to make function name more clojurish

--- desktop/src-common/play_clj_ground/entities.clj
+++ desktop/src-common/play_clj_ground/entities.clj
@@ -3,16 +3,24 @@
 
 (defprotocol MapP
   (get-viewport [this] "returns the viewport associated with the map")
-  (get-cell [this coords] "return the cell at given coordinates"))
+  (get-cell [this coords] "return the cell at given coordinates")
+  (get-objects-at [this coords] "return the objects at given coordinates"))
 
 (defprotocol ViewportP
   (get-coords [this] "get coordinates of viewport")
   (point-at [this coords] "center the viewport at given coordinates"))
 
+(defprotocol CellP
+  (get-texture [this] "returns texture for this cell")
+  (get-objects [this] "returns objects residing at this cell")
+  (set-texture [this new-texture] "sets the texture for this cell")
+  (modify-objects [this fn] "modifies objects in this cell using fn"))
+
 (deftype TiledMap [cells viewport]
   MapP
   (get-viewport [this] viewport)
-  (get-cell [this [x y]] nil))
+  (get-cell [this [x y]] nil)
+  (get-objects-at [this [x y]] nil))
 
 (deftype Viewport [^:volatile-mutable coords tiled-map]
   ViewportP
